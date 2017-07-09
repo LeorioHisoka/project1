4ab9e4057aa634a8fb6e04e623334e8e542e6b29
LeorioHisoka <LeorioHisoka@users.noreply.github.com>
2017-07-09 17:53:39 +0200

    مسح بعض الاسطور البسيطة
    فيه مشكلة فى دالة الضرب

--- AmirProgram2.cpp
+++ AmirProgram2.cpp
@@ -1,36 +1,38 @@
 #include <iostream>
 using namespace std;
-static long int wo, result, b;
+static long int  result;
 long int ao = 5;
-long int lss = ao;
-long int k = ao;
-long int mulInt(int value, long int wo);
-long int fact(long int so);
+long int ss ;
+long int e;
+long int am=ao;
+long int mulInt(long int value, long int wo);
+long int fact( long int so);
 int main(int argc, char const *argv[]) {
-  cout << fact(ao) << endl;
-  cin >> wo >> b;
-  result = wo;
-  cout << mulInt(b, wo) << endl;
-  return 0;
-}
-long int mulInt(int b, long int wo) {
-  b--;
-  int ss = wo;
-  /* if (wo == 0 || b == 0) {
-    return 0;
-} else*/ if (b > 0 && wo > 0) {
-    return mulInt(b, result + wo);
-  } else if (ss > 0 && b == 0) {
+cout <<"add= ";
+long int wa,c;
+cin >> wa >> c;
+result = wa;
+cout << mulInt(c, wa) << endl;
+cout <<"fact= ";
+cin >> ao;
+  fact( ao);
+  return 0;}
+long int mulInt(long int nn, long int wo) { //الدالة مش بتتعامل بالسالب
+  nn--;
+     ss = wo;
+  if (nn >= 1 && wo >= 1) {
+    return mulInt(nn, result + wo);
+  } else if (ss > 0 && nn == 0) {
     return ss;
   } else {
-    return 0;
-  }
-}
-long int fact(long int so) {
-  lss--;
-  if (lss > 0) {
-    return k = fact(mulInt(lss, so));
+    return 0;}}
+
+long int fact( long int so) {
+  am--;
+  if (am >= 1) {
+    int l = mulInt(am, so);
+    cout << l << endl;
+    return fact( l);
   } else {
-    return k;
-  }
-}
+    cout << so << endl;
+    return 0;}}
